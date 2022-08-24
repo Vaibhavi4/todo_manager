95b1da1689d7fee5e3d3fd365f7eafc92c73c18c
Vaibhavi Mandavia <vaibhavimandavia@FWS-CHE-LT-2568.local>
2022-08-24 17:25:24 +0530

    Modified routes.rb  with default rails routing convention

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "todos", to: "todos#index"
+  # get "todos", to: "todos#index"
+  # post "todos/create", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
+
+  resources :todos
 end
