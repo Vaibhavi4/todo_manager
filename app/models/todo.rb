b6059d8c0c26fb64d0e1098adad8fda83cdb512a
Vaibhavi Mandavia <vaibhavimandavia@FWS-CHE-LT-2568.local>
2022-08-24 17:35:32 +0530

    implemented class methods overdue, due_today and due_later

---
+++ app/models/todo.rb
@@ -0,0 +1,44 @@
+class Todo < ActiveRecord::Base
+
+    def due_today?
+        due_date == Date.today
+    end
+
+    def self.overdue
+        all.where("due_date < ?", Date.today)
+    end
+
+    def self.due_today
+        all.where("due_date = ?", Date.today)
+    end
+
+    def self.due_later
+        all.where("due_date > ?", Date.today)
+    end
+
+    def self.completed
+        all.where(completed: true)
+    end
+
+    def self.show_list
+        puts "My Todo List\n\n"
+        puts "Overdue\n"
+        puts overdue.show_list
+        puts "\n\n"
+
+        puts "Due Today\n"
+        puts due_today.showlist
+        puts "\n\n"
+
+        puts "Due Later\n"
+        puts due_later.showlist
+        puts "\n\n"
+    end
+
+    def self.mark_as_complete!(id)
+        todo = find(id)
+        todo.completed = true
+        todo.save!
+        todo
+    end
+end
