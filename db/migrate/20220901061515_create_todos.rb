80486c0cf16f7b957c6b5310e79dc3e7de8fefa0
Vaibhavi Mandavia <vaibhavimandavia@FWS-CHE-LT-2568.local>
2022-09-01 13:06:28 +0530

    Added migrations to create new todos and users table

---
+++ db/migrate/20220901061515_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.text :todo_text
+      t.date :due_date
+      t.boolean :completed
+      t.boolean :important
+
+      # t.timestamps
+    end
+  end
+end
