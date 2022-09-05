48c71b49a435e3d44b8e052fd6fd2367b6beb685
Vaibhavi Mandavia <vaibhavimandavia@FWS-CHE-LT-2568.local>
2022-09-05 16:07:39 +0530

    added signup and signin routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,15 @@ Rails.application.routes.draw do
   # get "todos/:id", to: "todos#show"
   get "todos/important", to: "todos#important" 
 
+  put "todos/important/:id", to: "todos#importantTodo"
+
   get "/" => "home#index"
 
   resources :todos
   resources :users
+
+  get "/signin" => "sessions#new", as: :new_sessions
+
+  post "/signin" => "sessions#create", as: :sessions
+
 end
